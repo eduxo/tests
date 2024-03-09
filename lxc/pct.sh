c93e5f43bf83d68672a0397653d314589031bb13
Mr.Eduxo <mr@eduxo.cz>
2024-03-09 14:26:28 +0100

    Update pct.sh

--- lxc/pct.sh
+++ lxc/pct.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This script prepare container debian:12 for exercise
 
-ID="103"
-HOSTNAME="103"
-IPv4="10.20.30.103/24"
+ID="101"
+HOSTNAME="101"
+IPv4="10.20.30.101/24"
 
 pct create "$ID" /var/lib/vz/template/cache/debian-12-standard_12.2-1_amd64.tar.zst \
     -arch amd64 \
@@ -12,6 +12,7 @@ pct create "$ID" /var/lib/vz/template/cache/debian-12-standard_12.2-1_amd64.tar.
     -cores 1 \
     -memory 512 \
     -swap 512 \
+    -password heslo \
     -rootfs volume=local-lvm:8 \
     -net0 name=eth0,bridge=vmbr1,gw=10.20.30.1,ip=$IPv4,type=veth  &&\
 pct start $ID &&\
@@ -22,11 +23,17 @@ pct exec $ID -- bash -c "apt update -y &&\
     systemctl start sshd &&\
     groupadd sysadmin &&\
     useradd -rm -d /home/sysadmin -s /bin/bash -g sysadmin -G sudo -u 1000 sysadmin &&\
-    sh -c 'echo "sysadmin:Netlab!23" | chpasswd'"
+    sh -c 'echo "sysadmin:Netlab!23" | chpasswd'
+    useradd -mU hogeuser &&\
+    echo "password" | passwd --stdin hogeuser"
 
 
 # --------------------------------SETTINGS FOR EXERCISES---------------------------------------
 
-
+# pct resize <id> rootfs <storage(ex: +4G)>
+# pct exec <id> -- mkdir -p /root/.ssh/
+# pct push <id> ~/.ssh/authorized_keys /root/.ssh/authorized_keys
+# -ssh-public-keys <filepath>
+#             Setup public SSH keys (one key per line, OpenSSH format).
 
 # --------------------------------SETTINGS FOR EXERCISES---------------------------------------
