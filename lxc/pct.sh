f9ba0d2f286794ea80a2291c4f77df64b90a14b2
Mr.Eduxo <mr@eduxo.cz>
2024-03-09 15:18:37 +0100

    Update pct.sh

--- lxc/pct.sh
+++ lxc/pct.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script prepare container debian:12 for exercise
+# https://pve.proxmox.com/pve-docs/pct.1.html
 
 # Container Settings
 ID="101"
@@ -30,8 +31,8 @@ sudo pct start $ID &&\
 sleep 10 &&\
 
 # Enable SSH Password Authentication
-sudo pct exec $ID -- sed -i 's/#PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sudo pct exec $ID -- systemctl restart sshd
+#sudo pct exec $ID -- sed -i 's/#PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+#sudo pct exec $ID -- systemctl restart sshd
 
 # Install sudo 
 sudo pct exec $ID -- apt update -y
@@ -45,8 +46,7 @@ sudo pct exec $ID -- sh -c 'echo "sysadmin:Netlab!23" | chpasswd'
 
 # --------------------------------SETTINGS FOR EXERCISES---------------------------------------
 
-sudo pct exec $ID -- apt update -y
-sudo pct exec $ID -- apt update -y
+# sudo pct exec $ID -- apt update -y
 
 # sudo pct exec $ID -- apt-get upgrade -y
 # sudo pct exec $ID -- apt-get autoremove -y
@@ -57,3 +57,8 @@ sudo pct exec $ID -- apt update -y
 # pct push <id> ~/.ssh/authorized_keys /root/.ssh/authorized_keys
 
 # --------------------------------SETTINGS FOR EXERCISES---------------------------------------
+
+# Edit /etc/hosts
+# echo -e '\e[0;92mPro nastaveni domain-name je nutne opravneni:\e[0m'
+# sudo sh -c 'echo "'$IPv4' '$HOSTNAME'.eduxo.lab '$HOSTNAME'
+# " >> /etc/hosts'
