14cf8840888553605fd02e4510157eee77cbb524
Mr.Eduxo <mr@eduxo.cz>
2024-03-09 13:43:34 +0100

    Update pct.sh

--- lxc/pct.sh
+++ lxc/pct.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 # This script prepare container debian:12 for exercise
 
-ID="102"
-HOSTNAME="name"
-IP="10.20.30.102/24"
-
-pct create $ID /var/lib/vz/template/cache/debian-12-standard_12.2-1_amd64.tar.zst \
+pct create 102 /var/lib/vz/template/cache/debian-12-standard_12.2-1_amd64.tar.zst \
     -arch amd64 \
     -ostype debian \
-    -hostname $NAME \
+    -hostname name \
     -cores 1 \
     -memory 512 \
     -swap 512 \
     -storage local-lvm \
-    -net0 name=eth0,bridge=vmbr1,gw=10.20.30.1,ip=$IP,type=veth  &&\
-pct start $ID &&\
+    -net0 name=eth0,bridge=vmbr1,gw=10.20.30.1,ip=10.20.30.102/24,type=veth  &&\
+pct start 102 &&\
 sleep 10 &&\
 
-pct exec $ID -- bash -c "apt update -y &&\
+pct exec 102 -- bash -c "apt update -y &&\
     apt install -y openssh-server &&\
     systemctl start sshd &&\
     useradd -mU sysadmin &&\
