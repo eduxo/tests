df6bb40503fdf935c240bef771d80b3e40f76caf
Mr.Eduxo <mr@eduxo.cz>
2024-03-10 12:23:06 +0100

    Update pct_template_exercise_ubuntu.sh

--- pct_template_exercise_ubuntu.sh
+++ pct_template_exercise_ubuntu.sh
@@ -44,8 +44,7 @@ sudo pct start $ID &&\
 sleep 10 &&\
 
 # Install sudo 
-sudo pct exec $ID -- apt update -y
-# sudo pct exec $ID -- apt install sudo -y
+sudo pct exec $ID -- apt update -y &&\
 
 # Add user to container
 sudo pct exec $ID -- groupadd sysadmin
@@ -53,6 +52,7 @@ sudo pct exec $ID -- useradd -rm -d /home/sysadmin -s /bin/bash -g sysadmin -G s
 sudo pct exec $ID -- sh -c 'echo "sysadmin:Netlab!23" | chpasswd'
 sudo pct exec $ID -- usermod -aG users sysadmin
 sudo pct exec $ID -- setcap cap_net_raw+p /bin/ping
+sudo pct push $ID -- mkdir /home/sysadmin/.ssh/
 sudo pct push $ID -- cp /root/.ssh/authorized_keys /home/sysadmin/.ssh/authorized_keys
 
 # --------------------------------SETTINGS FOR EXERCISES---------------------------------------
@@ -64,4 +64,4 @@ sudo pct push $ID -- cp /root/.ssh/authorized_keys /home/sysadmin/.ssh/authorize
 # Edit /etc/hosts
 sudo sh -c 'echo "'$IPv4' '$HOSTNAME' '$HOSTNAME'.eduxo.lab" >> /etc/hosts'
 
-ssh -A sysadmin@$HOSTNAME.eduxo.lab
+ssh -o "StrictHostKeyChecking no" sysadmin@$HOSTNAME.eduxo.lab
