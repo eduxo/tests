4d5093ed156d0c331dd70f41dfd691cbc26a6885
Tihlařík Jaroslav <jaroslav.tihlarik@cichnovabrno.cz>
2024-04-07 19:12:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ iface vmbr0 inet dhcp
         bridge-ports none
         bridge-stp off
         bridge-fd 0
-	bridge-maxwait 0
+        bridge-maxwait 0
 
 auto vmbr1
 iface vmbr1 inet static
@@ -36,7 +36,7 @@ iface vmbr1 inet static
         bridge-ports none
         bridge-stp off
         bridge-fd 0
-	bridge-maxwait 3
+        bridge-maxwait 3
 
         post-up   echo 1 > /proc/sys/net/ipv4/ip_forward
         post-up   iptables -t nat -A POSTROUTING -s '10.20.30.0/24' -o vmbr0 -j MASQUERADE
